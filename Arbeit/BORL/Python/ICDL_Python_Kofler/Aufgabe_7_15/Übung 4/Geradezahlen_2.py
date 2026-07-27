60d8309c7df0d591d5c7d4eb255e223db3d3b236
Koflaaa <taylor.kofler@itlabs.at>
2026-07-27 11:30:15 +0200

    Delete Arbeit/BORL directory

--- Arbeit/BORL/Python/ICDL_Python_Kofler/Aufgabe_7_15/Übung 4/Geradezahlen_2.py
+++
@@ -1,3 +0,0 @@
-def Version_2():
-    for i in range (0,100,2):
-        print(i)
--- Arbeit/BORL/Python/OOP/BibliothekOOP/autor_class.py
+++
@@ -1,6 +0,0 @@
-class Autor():      # Erstellen der Klasse Autor sammt Name
-    def __init__(self, name:str):
-        self.name = name
-
-    def __str__(self):
-        return f"{self.name}"
